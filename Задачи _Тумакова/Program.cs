47b9c25a6d4158c9065eeb92cfa107484a78753a
79046 <79046@LAPTOP-279GGFSI>
2024-11-21 21:08:03 +0300

    Добавьте файлы проекта.

---
+++ Задачи _Тумакова/Program.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задачи__Тумакова
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Упражнение 4.1) Написать программу, которая читает с экрана число от 1 до 365(номер дня
+            в году), переводит этот число в месяц и день месяца. Например, число 40 соответствует 9
+            февраля(високосный год не учитывать) */
+            Console.Write("Упражнение 4.1 \nВведите номер дня в году от 1 до 365: ");
+            int num = int.Parse(Console.ReadLine());
+            if ((1 <= num) && ( num <= 31))
+            {
+                Console.WriteLine($"{num} January");
+            }
+            else if ((32 <= num) && (num <= 59))
+            {
+                Console.WriteLine($"{num-31} February");
+            }
+            else if ((60 <= num) && (num <= 90))
+            {
+                Console.WriteLine($"{num - 59} March");
+            }
+            else if ((91 <= num) && (num <= 120))
+            {
+                Console.WriteLine($"{num - 90} April");
+            }
+            else if ((121 <= num) && (num <= 151))
+            {
+                Console.WriteLine($"{num - 120} May");
+            }
+            else if ((152 <= num) && (num <= 181))
+            {
+                Console.WriteLine($"{num - 151} June");
+            }
+            else if ((182 <= num) && (num <= 212))
+            {
+                Console.WriteLine($"{num - 181} July");
+            }
+            else if ((213 <= num) && (num <= 242))
+            {
+                Console.WriteLine($"{num - 212} August");
+            }
+            else if ((243 <= num) && (num <= 273))
+            {
+                Console.WriteLine($"{num - 242} September");
+            }
+            else if ((274 <= num) && (num <= 304))
+            {
+                Console.WriteLine($"{num - 273} October");
+            }
+            else if ((305 <= num) && (num <= 334))
+            {
+                Console.WriteLine($"{num - 304} November");
+            }
+            else
+            {
+                Console.WriteLine($"{num - 334} December");
+            }
+
+            /*Упражнение 4.2) Добавить к задаче из предыдущего упражнения проверку числа введенного
+            пользователем. Если число меньше 1 или больше 365, программа должна вырабатывать
+            исключение, и выдавать на экран сообщение */
+            Console.Write("\nУпражнение 4.2 \nВведите номер дня в году от 1 до 365: ");
+            int day = int.Parse(Console.ReadLine());
+            if ((1 <= day) && (day <= 31))
+            {
+                Console.WriteLine($"{day} January");
+            }
+            else if ((32 <= day) && (day <= 59))
+            {
+                Console.WriteLine($"{day - 31} February");
+            }
+            else if ((60 <= day) && (day <= 90))
+            {
+                Console.WriteLine($"{day - 59} March");
+            }
+            else if ((91 <= day) && (day <= 120))
+            {
+                Console.WriteLine($"{day - 90} April");
+            }
+            else if ((121 <= day) && (day <= 151))
+            {
+                Console.WriteLine($"{day - 120} May");
+            }
+            else if ((152 <= day) && (day <= 181))
+            {
+                Console.WriteLine($"{day - 151} June");
+            }
+            else if ((182 <= day) && (day <= 212))
+            {
+                Console.WriteLine($"{day - 181} July");
+            }
+            else if ((213 <= day) && (day <= 242))
+            {
+                Console.WriteLine($"{day - 212} August");
+            }
+            else if ((243 <= day) && (day <= 273))
+            {
+                Console.WriteLine($"{day - 242} September");
+            }
+            else if ((274 <= day) && (day <= 304))
+            {
+                Console.WriteLine($"{day - 273} October");
+            }
+            else if ((305 <= day) && (day <= 334))
+            {
+                Console.WriteLine($"{day - 304} November");
+            }
+            else if ((335 <= day) && (day <= 365))
+            {
+                Console.WriteLine($"{day - 334} December");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели неверное число, номер дня должен быть от 1 до 366");
+            }
+
+            /* Домашнее задание 4.1) Изменить программу из упражнений 4.1 и 4.2 так, чтобы она
+            учитывала год (високосный или нет). Год вводится с экрана. (Год високосный, если он
+            делится на четыре без остатка, но если он делится на 100 без остатка, это не високосный
+            год. Однако, если он делится без остатка на 400, это високосный год.) */
+            Console.WriteLine("\nДомашнее задание 4.1");
+            Console.Write("Введите год: ");
+            int year = int.Parse(Console.ReadLine());
+            Console.Write("Введите номер дня в году от 1 до 366: ");
+            int numDay = int.Parse(Console.ReadLine());
+            if ((year % 4 == 0) && (year % 100 != 0))
+                if ((1 <= numDay ) && (numDay <= 31))
+                {
+                    Console.WriteLine($"{numDay} January");
+                }
+                else if ((32 <= numDay) && (numDay <= 60))
+                {
+                    Console.WriteLine($"{numDay - 31} February");
+                }
+                else if ((61 <= numDay) && (numDay <= 91))
+                {
+                    Console.WriteLine($"{numDay - 60} March");
+                }
+                else if ((92 <= numDay) && (numDay <= 121))
+                {
+                    Console.WriteLine($"{numDay - 91} April");
+                }
+                else if ((122 <= numDay) && (numDay <= 152))
+                {
+                    Console.WriteLine($"{numDay - 121} May");
+                }
+                else if ((153 <= numDay) && (numDay <= 182))
+                {
+                    Console.WriteLine($"{numDay - 152} June");
+                }
+                else if ((183 <= numDay) && (numDay <= 213))
+                {
+                    Console.WriteLine($"{numDay - 182} July");
+                }
+                else if ((214 <= numDay) && (numDay <= 243))
+                {
+                    Console.WriteLine($"{numDay - 213} August");
+                }
+                else if ((244 <= numDay) && (numDay <= 274))
+                {
+                    Console.WriteLine($"{numDay - 243} September");
+                }
+                else if ((275 <= numDay) && (numDay <= 305))
+                {
+                    Console.WriteLine($"{numDay - 274} October");
+                }
+                else if ((306 <= numDay) && (numDay <= 335))
+                {
+                    Console.WriteLine($"{numDay - 305} November");
+                }
+                else if ((336 <= numDay) && (numDay <= 366))
+                {
+                    Console.WriteLine($"{numDay - 335} December");
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели неверное число, номер дня должен быть от 1 до 366");
+                }
+            else
+                if ((1 <= numDay) && (numDay <= 31))
+                {
+                    Console.WriteLine($"{numDay} January");
+                }
+                else if ((32 <= numDay) && (numDay <= 59))
+                {
+                    Console.WriteLine($"{numDay - 31} February");
+                }
+                else if ((60 <= numDay) && (numDay <= 90))
+                {
+                    Console.WriteLine($"{numDay - 59} March");
+                }
+                else if ((91 <= numDay) && (numDay <= 120))
+                {
+                    Console.WriteLine($"{numDay - 90} April");
+                }
+                else if ((121 <= numDay) && (numDay <= 151))
+                {
+                    Console.WriteLine($"{numDay - 120} May");
+                }
+                else if ((152 <= numDay) && (numDay <= 181))
+                {
+                    Console.WriteLine($"{numDay - 151} June");
+                }
+                else if ((182 <= numDay) && (numDay <= 212))
+                {
+                    Console.WriteLine($"{numDay - 181} July");
+                }
+                else if ((213 <= numDay) && (numDay <= 242))
+                {
+                    Console.WriteLine($"{numDay - 212} August");
+                }
+                else if ((243 <= numDay) && (numDay <= 273))
+                {
+                    Console.WriteLine($"{numDay - 242} September");
+                }
+                else if ((274 <= numDay) && (numDay <= 304))
+                {
+                    Console.WriteLine($"{numDay - 273} October");
+                }
+                else if ((305 <= numDay) && (numDay <= 334))
+                {
+                    Console.WriteLine($"{numDay - 304} November");
+                }
+                else if ((335 <= numDay) && (numDay <= 365))
+                {
+                    Console.WriteLine($"{numDay - 334} December");
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели неверное число, номер дня должен быть от 1 до 366");
+                }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задачи_файла/Program.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задачи_файла
+{
+    internal class Program
+    {
+        enum Week
+        {
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        static void Main(string[] args)
+        {
+            /* 1) Дана последовательность из 10 чисел. Определить, является ли эта последовательность
+            упорядоченной по возрастанию. В случае отрицательного ответа определить
+            порядковый номер первого числа, которое нарушает данную последовательность.
+            Использовать break */
+            Console.WriteLine("Номер 1) \nВведите последовательность из 10 чисел");
+            Console.Write("Введите певрое число: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе  число: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.Write("Введите третье число: ");
+            int num3 = int.Parse(Console.ReadLine());
+            Console.Write("Введите четвертое число: ");
+            int num4 = int.Parse(Console.ReadLine());
+            Console.Write("Введите пятое число: ");
+            int num5 = int.Parse(Console.ReadLine());
+            Console.Write("Введите шестое число: ");
+            int num6 = int.Parse(Console.ReadLine());
+            Console.Write("Введите седьмое число: ");
+            int num7 = int.Parse(Console.ReadLine());
+            Console.Write("Введите восьмое число: ");
+            int num8 = int.Parse(Console.ReadLine());
+            Console.Write("Введите девятое число: ");
+            int num9 = int.Parse(Console.ReadLine());
+            Console.Write("Введите десятое число: ");
+            int num10 = int.Parse(Console.ReadLine());
+            int[] sequence = { num1, num2, num3, num4, num5, num6, num7, num8, num9, num10 };
+            bool up = true;
+            int i = 0;
+            while ((i < 9) && (up))
+            {
+                if (sequence[i] >= sequence[i + 1])
+                {
+                    up = false;
+                    break;
+                }
+                i++;
+            }
+            if (up)
+            {
+                Console.WriteLine("Последовательность возрастает");
+            }
+            else
+            {
+                int index = 0;
+                for (int j = 0; j < 10; j++)
+                {
+                    if (sequence[j] >= sequence[j + 1])
+                    {
+                        index = j + 1;
+                        break;
+                    }
+                }
+                Console.WriteLine($"Последовательность не возрастает. Последовательность нарушает {index + 1} число.");
+            }
+
+            /*2. Игральным картам условно присвоены следующие порядковые номера в зависимости от
+            их достоинства: «валету» — 11, «даме» — 12, «королю» — 13, «тузу» — 14.
+            Порядковые номера остальных карт соответствуют их названиям («шестерка»,
+            «девятка» и т. п.). По заданному номеру карты k (6 <=k <= 14) определить достоинство
+            соответствующей карты. Использовать try-catch-finally */
+            Console.WriteLine("\nНомер 2)");
+            Console.Write("Введите номер карты от 6 до 14: ");
+            int k = int.Parse(Console.ReadLine());
+            if ( k < 6 || k > 14)
+            {
+                Console.WriteLine("Некорректный ввод, повторите попытку");
+            }
+            else
+            {
+                try
+                {
+                    switch (k)
+                    {
+                        case 6:
+                            Console.WriteLine("Ваша карта - шестерка"); break;
+                        case 7:
+                            Console.WriteLine("Ваша карта - семерка"); break;
+                        case 8:
+                            Console.WriteLine("Ваша карта - восьмерка"); break;
+                        case 9:
+                            Console.WriteLine("Ваша карта - девятка"); break;
+                        case 10:
+                            Console.WriteLine("Ваша карта - десятка"); break;
+                        case 11:
+                            Console.WriteLine("Ваша карта - валет"); break;
+                        case 12:
+                            Console.WriteLine("Ваша карта - дама"); break;
+                        case 13:
+                            Console.WriteLine("Ваша карта - король"); break;
+                        case 14:
+                            Console.WriteLine("Ваша карта - туз"); break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Некорректный ввод, повторите попытку");
+                }
+            }
+            
+
+            /* 3) Напишите программу, которая принимает на входе строку и производит выходные
+            данные в соответствии со следующей таблицей:*/
+            Console.WriteLine("\nНомер 3)");
+            Console.WriteLine("Введите значение из таблицы");
+            string name = Console.ReadLine();
+            switch (name.ToLower())
+            {
+                case "jabroni":
+                    Console.WriteLine("Patron Tequila");break;
+                case "school counselor":
+                    Console.WriteLine("Anything with Alcohol");break;
+                case "programmer":
+                    Console.WriteLine("Hipster Craft Beer");break;
+                case "bike gang member":
+                    Console.WriteLine("Moonshine");break;
+                case "politician":
+                    Console.WriteLine("Your tax dollars");break;
+                case "rapper":
+                    Console.WriteLine("Cristal");break;
+            }
+            if ((name.ToLower() != "jabroni") && (name.ToLower() != "school counselor") && (name.ToLower() != "programmer") && (name.ToLower() != "bike gang member") && (name.ToLower() != "politician") && (name.ToLower() != "rapper"))
+            {
+                Console.WriteLine("Beer");
+            }
+
+            /*4. Составить программу, которая в зависимости от порядкового номера дня недели (1, 2,
+            ...,7) выводит на экран его название (понедельник, вторник, ..., воскресенье).
+            Использовать enum.*/
+            Console.WriteLine("\nНомер 4)");
+            Console.WriteLine("Введите день недели по счёту: ");
+            int weekDay = int.Parse(Console.ReadLine());
+            Week currentDay = (Week)weekDay;
+            switch (currentDay)
+            {
+                case Week.Monday:
+                    Console.WriteLine("Понедельник");break;
+                case Week.Tuesday:
+                    Console.WriteLine("Вторник");break;
+                case Week.Wednesday:
+                    Console.WriteLine("Среда");break;
+                case Week.Thursday:
+                    Console.WriteLine("Четверг");break;
+                case Week.Friday:
+                    Console.WriteLine("Пятница");break;
+                case Week.Saturday:
+                    Console.WriteLine("Суббота");break;
+                case Week.Sunday:
+                    Console.WriteLine("Воскресенье");break;
+            }
+            if ((weekDay < 1) || (weekDay > 7))
+            {
+                Console.WriteLine("Номер дня недели введен неправильно");
+            }
+
+            /*5. Создать массив строк. При помощи foreach обойти весь массив. При встрече элемента
+            "Hello Kitty" или "Barbie doll" необходимо положить их в “сумку”, т.е. прибавить к
+            результату. Вывести на экран сколько кукол в “сумке”.*/
+            Console.WriteLine("\nНомер 5)");
+            string[] array = { "Hello Kitty", "Barbie doll", "Man", "Women", "Cat", "a", "qwerty" };
+            int sumka = 0;
+            foreach (string s in array)
+            {
+                if ((s == "Hello Kitty") || (s == "Barbie doll"))
+                {
+                    sumka++;
+                }
+            }
+            Console.WriteLine($"В сумке {sumka} кукол");
+            Console.ReadKey();
+        }
+    }
+}
